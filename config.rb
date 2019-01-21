aa8e252b204911e7212bf47a86feb37899e7f0c0
pnts <hi@pnts.us>
2019-01-21 00:04:15 -0500

    coaching pilot program page

--- config.rb
+++ config.rb
@@ -5,8 +5,7 @@ activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
 
-activate :relative_assets
-set :relative_links, true
+activate :directory_indexes
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
